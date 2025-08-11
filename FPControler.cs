eb1a623d66e76543fae16ed3b6beb0f9713be52c
Emre Guzel <136666281+PrimeEmre@users.noreply.github.com>
2025-08-11 18:40:12 +0000

    aded forward and backword

---
+++ FPControler.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPControler : MonoBehaviour
+{
+    float speed = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+void FixedUpdate()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+
+        transform.position += new Vector3(x, 0, z) * speed;
+    }
+
+}

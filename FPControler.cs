85fd8fb21c8e9fe95a1aeb4b0f0a5e53fb5941d6
Emre Guzel <136666281+PrimeEmre@users.noreply.github.com>
2025-08-14 14:21:48 +0000

    added the mouse scroele and the speed of the mouse

--- FPControler.cs
+++ FPControler.cs
@@ -4,25 +4,42 @@ using UnityEngine;
 
 public class FPControler : MonoBehaviour
 {
+    public GameObject cam;
     float speed = 0.1f;
+    float Xsensitivity =  5;
+    float Ysensitivity = 5;
     Rigidbody rb;
     CapsuleCollider capsule;
+
+    Quaternion cameraRot;
+    Quaternion charecterRot;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = this.GetComponent<Rigidbody>();
+        capsule = this.GetComponent<CapsuleCollider>();
+        cameraRot = cam.transform.rotation;
+        charecterRot = this.transform.localRotation;
     }
 
     // Update is called once per frame
     void Update()
     {
-        rb = this.GetComponent<Rigidbody>();
-        capsule = this.GetComponent<CapsuleCollider>();
+       
 
     }
 void FixedUpdate()
     {
+        float yRot = Input.GetAxis("Mouse X") *Ysensitivity;
+        float xRot = Input.GetAxis("Mouse Y") *Xsensitivity;
+
+        cameraRot *= Quaternion.Euler(-xRot, 0, 0);
+        charecterRot *= Quaternion.Euler( 0, yRot, 0);
+
+        this.transform.localRotation = charecterRot;
+        cam.transform.localRotation = cameraRot;
+
         if(Input.GetKeyDown (KeyCode.Space) && Isgrounded())
         rb.AddForce(0, 300, 0);
        
